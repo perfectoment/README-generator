29cecff6ef5c77cf2107864c42f2d83713354fa3
Lee Marvin <ryenem8@gmail.com>
2020-10-08 15:38:38 -0700

    added generate markdown and write file functionality

--- index.js
+++ index.js
@@ -1,68 +1,73 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const utils = require("utils");
+const util = require("util");
+const axios = require("axios");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What would you like to name your README?",
-      name: "title"
-    },
-    {
-      type: "input",
-      message: "Please enter a description of your project",
-      name: "description"
-    },
-    {
-      type: "input",
-      message: "How do you install your project?",
-      name: "install",
-    },
-    {
-        type: "input",
-        message: "How do you use your project?",
-        name: "usage",
-    },
-    {
-        type: "input",
-        message: "License?",
-        name: "license",
-    },
-    {
-        type: "input",
-        message: "Who contributed?",
-        name: "contributors",
-    },
-    {
-        type: "input",
-        message: "test",
-        name: "test",
-    },
-    {
-        type: "input",
-        message: "Questions",
-        name: "questions",
-    },
-  ])
-  .then(function(response) {
+const writeFileAsync = util.promisify(fs.writeFile);
 
-    
-  })
-
-
-
-
-
-const questions = [
-
-];
 
 function writeToFile(fileName, data) {
+    writeFileAsync(fileName, generateMarkdown(data), "utf8")
 }
 
 function init() {
-
+    return inquirer.prompt([
+        {
+          type: "input",
+          message: "What would you like to name your README?",
+          name: "title"
+        },
+        {
+          type: "input",
+          message: "Please enter a description of your project",
+          name: "description"
+        },
+        {
+          type: "input",
+          message: "How do you install your project?",
+          name: "install",
+        },
+        {
+            type: "input",
+            message: "How do you use your project?",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "License?",
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "Who contributed?",
+            name: "contributors",
+        },
+        {
+            type: "input",
+            message: "Tests?",
+            name: "test",
+        },
+        {
+            type: "input",
+            message: "Github profile picture?",
+            name: "Githubpics",
+        },
+        {
+            type: "input",
+            message: "Github email?",
+            name: "Githubemail",
+        },
+      ])
+      
+      .then(function(data) {
+        axios.get()
+        writeToFile("README.md", data)
+        
+        })
+    
 }
+
+
 
 init();
